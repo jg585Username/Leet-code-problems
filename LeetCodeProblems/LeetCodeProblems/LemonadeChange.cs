4161277aaa2211cb45aeb5de57f6f2058982e807
jg585Username <jonathan.sguan06@gmail.com>
2025-01-16 01:04:14 -0800

    Update LemonadeChange.cs

--- LeetCodeProblems/LeetCodeProblems/LemonadeChange.cs
+++ LeetCodeProblems/LeetCodeProblems/LemonadeChange.cs
@@ -2,5 +2,42 @@ namespace LeetCodeProblems;
 
 public class LemonadeChange
 {
-    
+    public bool lemonadeChange(int[] bills) {
+        int five = 0;
+        int ten = 0;
+        for (int i = 0; i < bills.Length; i++){
+
+
+            if (bills[i] == 5){
+                five++;
+            }
+
+            if (bills[i] == 10){
+                ten++;
+            }
+
+            if (bills[i] - 5 == 5){
+                five--;
+            }
+            if (bills[i] - 5 == 15){
+                if (ten <= 0 && five >= 3){
+                    five = five - 3;
+                }
+                else {
+                    five--;
+                    ten--;
+                }
+            }
+
+            if (five < 0 || ten < 0){
+                return false;
+            }
+
+
+
+
+        }
+        return true;
+
+    }
 }
