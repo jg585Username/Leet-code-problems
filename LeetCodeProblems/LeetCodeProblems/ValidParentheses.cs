2ed700eeccbfdff6e94e53f3548fa1e853f3a2c6
jg585Username <jonathan.sguan06@gmail.com>
2025-01-16 01:50:49 -0800

    Update ValidParentheses.cs

--- LeetCodeProblems/LeetCodeProblems/ValidParentheses.cs
+++ LeetCodeProblems/LeetCodeProblems/ValidParentheses.cs
@@ -2,5 +2,23 @@ namespace LeetCodeProblems;
 
 public class ValidParentheses
 {
-    
+    public bool IsValid(string s) {
+        Stack<char> stack = new Stack<char>();
+        foreach (char c in s) {
+            if (c == '(' || c == '{' || c == '[') {
+                // Push opening brackets onto the stack.
+                stack.Push(c);
+            } else {
+                if (stack.Count == 0) return false;
+
+                char top = stack.Pop();
+
+                if (c == ')' && top != '(') return false;
+                if (c == '}' && top != '{') return false;
+                if (c == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
 }
