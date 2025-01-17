63f4309c8a4cb13d5f514e1163033d0493617501
jg585Username <jonathan.sguan06@gmail.com>
2025-01-16 16:48:36 -0800

    Update RemoveDuplicates.cs

--- LeetCodeProblems/LeetCodeProblems/RemoveDuplicates.cs
+++ LeetCodeProblems/LeetCodeProblems/RemoveDuplicates.cs
@@ -2,5 +2,22 @@ namespace LeetCodeProblems;
 
 public class RemoveDuplicates
 {
-    
+    public int RemoveDup(int[] nums) { 
+        //nums is in ascending order
+
+        if (nums == null || nums.Length == 0) {
+            return 0;
+        }
+
+        int k = 1;
+        int [] duplicates = new int[nums.Length];
+        for (int i = 1; i < nums.Length; i++){
+            if (nums [i] != nums [i - 1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        
+        return k;
+    }
 }
