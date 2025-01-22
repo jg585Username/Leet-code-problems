a2efd9158c9a582ad6b482921b1d90f89c3610cf
jg585Username <jonathan.sguan06@gmail.com>
2025-01-21 23:54:28 -0800

    Add RemoveElement.cs

--- LeetCodeProblems/LeetCodeProblems/obj/Debug/net9.0/LeetCodeProblems.AssemblyInfo.cs
+++ LeetCodeProblems/LeetCodeProblems/obj/Debug/net9.0/LeetCodeProblems.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fb16cc65c4872d75c8028d55ff16a1c3d60de7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52dee0cb54e9086e469bc655b0a962d925a4f5e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeetCodeProblems")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
