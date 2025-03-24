c1f461fe8845481daa80917d1a79205b459165ba
jg585Username <jonathan.sguan06@gmail.com>
2025-03-23 20:25:19 -0700

    Update SearchInsertPosition.cs

--- LeetCodeProblems/LeetCodeProblems/SearchInsertPosition.cs
+++ LeetCodeProblems/LeetCodeProblems/SearchInsertPosition.cs
@@ -2,5 +2,22 @@ namespace LeetCodeProblems;
 
 public class SearchInsertPosition
 {
-    
+    public int SearchInsert(int[] nums, int target) { //binary search
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return left;
+    }
 }
