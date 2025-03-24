0c00b644dd2bec0378e2529649e430de2f6c0973
jg585Username <jonathan.sguan06@gmail.com>
2025-03-23 20:27:13 -0700

    Update LengthOfLastWord.cs

--- LeetCodeProblems/LeetCodeProblems/LengthOfLastWord.cs
+++ LeetCodeProblems/LeetCodeProblems/LengthOfLastWord.cs
@@ -2,5 +2,19 @@ namespace LeetCodeProblems;
 
 public class LengthOfLastWord
 {
-    
+    public int LenOfLastWord(string s) {
+        int count = 0;
+        int i = s.Length - 1;
+
+        while (i >= 0 && s[i] == ' ') {
+            i--;
+        }
+
+        while (i >= 0 && s[i] != ' ') {
+            count++;
+            i--;
+        }
+
+        return count;
+    }
 }
