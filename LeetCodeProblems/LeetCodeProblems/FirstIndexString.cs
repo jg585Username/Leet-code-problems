50297ad69ddb370e62ce7cfa3cac3203761fa382
jg585Username <jonathan.sguan06@gmail.com>
2025-03-23 20:24:20 -0700

    Update FirstIndexString.cs

--- LeetCodeProblems/LeetCodeProblems/FirstIndexString.cs
+++ LeetCodeProblems/LeetCodeProblems/FirstIndexString.cs
@@ -2,5 +2,8 @@ namespace LeetCodeProblems;
 
 public class FirstIndexString
 {
-    
+    public int StrStr(string haystack, string needle) {
+        int index = haystack.IndexOf(needle);
+        return index;
+    }
 }
