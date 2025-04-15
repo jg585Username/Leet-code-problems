d933d2ab16f8a1c51beaffcc555ddc4a799a91c0
jg585Username <jonathan.sguan06@gmail.com>
2025-04-14 22:28:40 -0700

    Update ClimbStairs.cs

--- LeetCodeProblems/LeetCodeProblems/ClimbStairs.cs
+++ LeetCodeProblems/LeetCodeProblems/ClimbStairs.cs
@@ -2,5 +2,18 @@ namespace LeetCodeProblems;
 
 public class ClimbStairs
 {
-    
+    public int Solution(int n) {
+        if (n <= 1) return 1;
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
 }
