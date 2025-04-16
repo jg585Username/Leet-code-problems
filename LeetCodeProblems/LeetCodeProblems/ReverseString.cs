c79d1bd94a659ff6731a6e3cc6df1cc106364d07
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 16:38:52 -0700

    Update ReverseString.cs

--- LeetCodeProblems/LeetCodeProblems/ReverseString.cs
+++ LeetCodeProblems/LeetCodeProblems/ReverseString.cs
@@ -2,5 +2,15 @@ namespace LeetCodeProblems;
 
 public class ReverseString
 {
-    
+    public void Solution(char[] s) {
+        int front = 0;
+        int back = s.Length - 1;
+        while (front < back){
+            char temp  = s[front];
+            s[front] = s[back];
+            s[back] = temp;
+            front++;
+            back--;
+        }
+    }
 }
