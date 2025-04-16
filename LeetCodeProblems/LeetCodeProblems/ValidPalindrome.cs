9c48fa5cc6c87a791d864dc4feac32728c8e92d6
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 16:27:00 -0700

    Update ValidPalindrome.cs

--- LeetCodeProblems/LeetCodeProblems/ValidPalindrome.cs
+++ LeetCodeProblems/LeetCodeProblems/ValidPalindrome.cs
@@ -2,5 +2,22 @@ namespace LeetCodeProblems;
 
 public class ValidPalindrome
 {
-    
+    public bool IsPalindrome(string s) {
+        int back = s.Length-1;
+        int front = 0;
+        while (front < back){
+            while (front < back && !char.IsLetterOrDigit(s[front])) {
+                front++;
+            }
+            while (front < back && !char.IsLetterOrDigit(s[back])) {
+                back--;
+            }
+            if (char.ToLower(s[front]) != char.ToLower(s[back])){
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
 }
