ce3695f54c9b09fbf3cf9343e0b4c73875cdccb5
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 17:19:54 -0700

    Update ValidPalindromeII.cs

--- LeetCodeProblems/LeetCodeProblems/ValidPalindromeII.cs
+++ LeetCodeProblems/LeetCodeProblems/ValidPalindromeII.cs
@@ -2,5 +2,28 @@ namespace LeetCodeProblems;
 
 public class ValidPalindromeII
 {
-    
+    public bool ValidPalindrome(string s) {
+        int front = 0;
+        int back = s.Length - 1;
+        int count = 0;
+        
+        bool IsPalindromeRange(string s, int left, int right) {
+            while (left < right) {
+                if (s[left] != s[right]) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        while (front < back){
+            if (s[front] != s[back]){
+                return IsPalindromeRange(s, front + 1, back) || IsPalindromeRange(s, front, back - 1);
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
 }
