86f318e7c8b9a8305c90ed0460bd3bf2994fb782
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 17:33:05 -0700

    Update ValidAnagram.cs

--- LeetCodeProblems/LeetCodeProblems/ValidAnagram.cs
+++ LeetCodeProblems/LeetCodeProblems/ValidAnagram.cs
@@ -2,5 +2,23 @@ namespace LeetCodeProblems;
 
 public class ValidAnagram
 {
-    
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+
+        int[] count = new int[26];
+        
+        foreach (char c in s) {
+            count[c - 'a']++;
+        }
+
+        foreach (char c in t) {
+            count[c - 'a']--;
+        }
+
+        foreach (int x in count) {
+            if (x != 0) return false;
+        }
+
+        return true;
+    }
 }
