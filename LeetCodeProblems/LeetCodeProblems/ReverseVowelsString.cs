5de0f457cd188424fe174a75ebe4b65f3dbb9faf
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 17:05:44 -0700

    Update ReverseVowelsString.cs

--- LeetCodeProblems/LeetCodeProblems/ReverseVowelsString.cs
+++ LeetCodeProblems/LeetCodeProblems/ReverseVowelsString.cs
@@ -2,5 +2,24 @@ namespace LeetCodeProblems;
 
 public class ReverseVowelsString
 {
-    
+    public string ReverseVowels(string s) {
+        int front = 0;
+        int back = s.Length - 1;
+        char[] chars = s.ToCharArray();
+        bool IsVowel(char c) {
+            return "aeiouAEIOU".Contains(c);
+        }
+        while (front < back){
+            while (front < back && !IsVowel(chars[front])) front++;
+            while (front < back && !IsVowel(chars[back])) back--;
+            if (IsVowel(char.ToLower(s[front])) && IsVowel(char.ToLower(s[back]))){
+                char temp = chars[front];
+                chars[front] = chars[back];
+                chars[back] = temp;
+                front++;
+                back--;
+            }
+        }
+        return new string(chars);
+    }
 }
