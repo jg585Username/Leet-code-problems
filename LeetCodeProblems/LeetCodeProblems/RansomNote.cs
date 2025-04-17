21bfcf173f9712a89973b20fc940513f6c11f5e3
jg585Username <jonathan.sguan06@gmail.com>
2025-04-16 17:44:01 -0700

    Update RansomNote.cs

--- LeetCodeProblems/LeetCodeProblems/RansomNote.cs
+++ LeetCodeProblems/LeetCodeProblems/RansomNote.cs
@@ -2,5 +2,26 @@ namespace LeetCodeProblems;
 
 public class RansomNote
 {
-    
+    public bool CanConstruct(string ransomNote, string magazine) {
+        if (ransomNote.Length > magazine.Length){
+            return false;
+        }
+
+        int [] count = new int [26];
+
+        foreach (char c in magazine){
+            count[c - 'a']++;
+        }
+
+        foreach (char c in ransomNote){
+            count[c - 'a']--;
+        }
+
+        for (int i =0; i < count.Length; i++){
+            if (count[i] < 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
