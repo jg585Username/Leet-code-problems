9d7cb794d3f66a0651f3dfbcfd0fab8797754e79
jg585Username <jonathan.sguan06@gmail.com>
2025-04-18 16:23:11 -0700

    Update BuyAndSell.cs

--- LeetCodeProblems/LeetCodeProblems/BuyAndSell.cs
+++ LeetCodeProblems/LeetCodeProblems/BuyAndSell.cs
@@ -2,5 +2,18 @@ namespace LeetCodeProblems;
 
 public class BuyAndSell
 {
-    
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 0; i < prices.Length; i++){
+            if (prices[i] < minPrice){
+                minPrice = prices[i];
+            }
+            else {
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.Max(maxProfit, profit);
+            }
+        }
+        return maxProfit;
+    }
 }
